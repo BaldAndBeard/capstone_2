d54436aea608ace1044711f3b1d2a95e59d3e894
Jay Eros <ugstrings@gmail.com>
2024-03-06 18:22:09 -0500

    Populated the TransferService and AccountService Classes

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -1,11 +1,15 @@
 package com.techelevator.tenmo.services;
 
 import com.techelevator.tenmo.model.Account;
+import com.techelevator.util.BasicLogger;
+import org.springframework.http.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 public class AccountService {
 
-    public static final String API_BASE_URL = "http://localhost:8080";
+    public static final String API_BASE_URL = "http://localhost:8080/Account/";
     private RestTemplate restTemplate = new RestTemplate();
 
     private String authToken = null;
@@ -17,24 +21,76 @@ public class AccountService {
 
     public Account getAccount(int id) {
         Account account = null;
+
+        try {
+            ResponseEntity<Account> response = restTemplate.exchange(API_BASE_URL + id, HttpMethod.GET, makeAuthEntity(), Account.class);
+            account = response.getBody();
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+
         return  account;
 
     }
+    // Define the url path for finding by userID
     public Account getAccountbyUserID( int userID) {
         Account accountByUserID = null;
+
+        try {
+            ResponseEntity<Account> response = restTemplate.exchange(API_BASE_URL + "?user_id=" + userID, HttpMethod.GET, makeAuthEntity(), Account.class);
+            accountByUserID = response.getBody();
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+
         return accountByUserID;
     }
-    public Account updateAccount(int accountId, Account accountToUpdate ) {
+    public Account updateAccount(Account accountToUpdate ) {
         Account updatedAccount = null;
+        HttpEntity<Account> entity = makeAccountEntity(accountToUpdate);
+
+        try {
+            ResponseEntity<Account> response = restTemplate.exchange(API_BASE_URL, HttpMethod.PUT, entity, Account.class);
+            updatedAccount = response.getBody();
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+
         return updatedAccount;
     }
     public Account createAccount(Account newAccount) {
         Account createdAccount = null;
+        HttpEntity<Account> entity = makeAccountEntity(newAccount);
+
+        try {
+            ResponseEntity<Account> response = restTemplate.exchange(API_BASE_URL, HttpMethod.POST, entity, Account.class);
+            createdAccount = response.getBody();
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+
         return createdAccount;
 
     }
     public void  deleteAccount( int accountID) {
+        try {
+            restTemplate.delete(API_BASE_URL + accountID, HttpMethod.DELETE, makeAuthEntity(), void.class);
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+    }
 
+    private HttpEntity<Void> makeAuthEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(authToken);
+        return new HttpEntity<>(headers);
+    }
+
+    private HttpEntity<Account> makeAccountEntity(Account account) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(authToken);
+        return new HttpEntity<>(account, headers);
     }
 
 
--- tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -1,12 +1,15 @@
 package com.techelevator.tenmo.services;
 
-import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.util.BasicLogger;
+import org.springframework.http.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 public class TransferService {
 
-    public static final String API_BASE_URL = "http://localhost:8080";
+    public static final String API_BASE_URL = "http://localhost:8080/transfer";
     private RestTemplate restTemplate = new RestTemplate();
 
     private String authToken = null;
@@ -19,22 +22,82 @@ public class TransferService {
    public Transfer getTransfer(int id) {
         Transfer transfer = null;
 
+       try {
+           ResponseEntity<Transfer> response = restTemplate.exchange(API_BASE_URL + "/" + id, HttpMethod.GET, makeAuthEntity(), Transfer.class);
+           transfer = response.getBody();
+       } catch (RestClientResponseException | ResourceAccessException e) {
+           BasicLogger.log(e.getMessage());
+       }
+
         return transfer;
 
     }
     public Transfer[] getAllTransfersByUserID(int userId) {
         Transfer[] transfers = null;
 
+        try {
+            ResponseEntity<Transfer[]> response = restTemplate.exchange(API_BASE_URL + "/" + userId, HttpMethod.GET, makeAuthEntity(), Transfer[].class);
+            transfers = response.getBody();
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+
         return transfers;
     }
-    public Transfer requestTransfer(int requestUserID, double amount){
+
+    public Transfer addTransfer(Transfer transferToAdd) {
+        Transfer newTransfer = null;
+        HttpEntity<Transfer> entity = makeTransferEntity(transferToAdd);
+
+        try {
+            ResponseEntity<Transfer> response = restTemplate.exchange(API_BASE_URL, HttpMethod.POST, entity, Transfer.class);
+            newTransfer = response.getBody();
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+
+        return newTransfer;
+    }
+
+    public Transfer requestTransfer(Transfer transferToRequest){
         Transfer requestedTransfer = null;
+        HttpEntity<Transfer> entity = makeTransferEntity(transferToRequest);
+
+        try {
+            ResponseEntity<Transfer> response = restTemplate.exchange(API_BASE_URL, HttpMethod.POST, entity, Transfer.class);
+            requestedTransfer = response.getBody();
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
 
         return requestedTransfer;
     }
      public Transfer[] getAllPendingTransfers() {
         Transfer[] allPendingTransfers = null;
 
+         try {
+             ResponseEntity<Transfer[]> response = restTemplate.exchange(API_BASE_URL + "?transfer_status='Pending'", HttpMethod.GET, makeAuthEntity(), Transfer[].class);
+             allPendingTransfers = response.getBody();
+         } catch (RestClientResponseException | ResourceAccessException e) {
+             BasicLogger.log(e.getMessage());
+         }
+
+
         return allPendingTransfers;
      }
+
+
+    private HttpEntity<Void> makeAuthEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(authToken);
+        return new HttpEntity<>(headers);
+    }
+
+    private HttpEntity<Transfer> makeTransferEntity(Transfer transfer) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(authToken);
+        return new HttpEntity<>(transfer, headers);
+    }
+
 }
